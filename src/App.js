62ff81ebb02c36a382e20e72a48f9f6853d6be51
Lokesh Manchanda <lokesh.8055.m@gmail.com>
2021-08-29 12:21:56 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import axios from "axios";
+import { useGoogleLogin, useGoogleLogout } from "react-google-login";
+import { useCallback, useEffect } from "react";
+
+export default function App() {
+  const fetchData = useCallback(async () => {
+    try {
+      const { data } = await axios.get(
+        "https://www.googleapis.com/calendar/v3"
+      );
+      console.log(data);
+    } catch (e) {}
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  const onSuccess = (param) => {
+    console.log("success", param);
+  };
+
+  const onFailure = (param) => {
+    console.log("failure", param);
+  };
+
+  const { signIn } = useGoogleLogin({
+    onSuccess,
+    onFailure,
+    clientId: `452890721843-bvp31s2cq988jsiu9mlh83elp7cs8s1u.apps.googleusercontent.com`,
+    isSignedIn: false,
+    accessType: "offline"
+    // scope: ''
+    // responseType: "code",
+    // prompt: "consent",
+  });
+
+  // useEffect(() => {
+  //   function start() {
+  //     // 2. Initialize the JavaScript client library.
+  //     gapi.client
+  //       .init({
+  //         apiKey: "YOUR_API_KEY",
+  //         // clientId and scope are optional if auth is not required.
+  //         clientId: "YOUR_WEB_CLIENT_ID.apps.googleusercontent.com",
+  //         scope: "profile"
+  //       })
+  //       .then(function () {
+  //         // 3. Initialize and make the API request.
+  //         return gapi.client.request({
+  //           path:
+  //             "https://people.googleapis.com/v1/people/me?requestMask.includeField=person.names"
+  //         });
+  //       })
+  //       .then(
+  //         function (response) {
+  //           console.log(response.result);
+  //         },
+  //         function (reason) {
+  //           console.log("Error: " + reason.result.error.message);
+  //         }
+  //       );
+  //   }
+  //   // 1. Load the JavaScript client library.
+  //   gapi.load("client", start);
+  // }, []);
+
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <button onClick={signIn}>Google</button>
+    </div>
+  );
+}
