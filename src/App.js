48b6534409fe1a01f1895e6f2ba6f64e1be12090
Lokesh Manchanda <lokesh.manchanda@jamsfy.com>
2021-12-22 19:39:36 +0530

    organized calendar events

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import axios from "axios";
 import { useGoogleLogin, useGoogleLogout } from "react-google-login";
 import { useCallback, useEffect, useState } from "react";
+import { addDays, format, isValid } from "date-fns";
 
 export default function App() {
-  const [data, setData] = useState({});
+  const [data, setData] = useState(null);
+  const [calendarData, setCalendarData] = useState({});
+  const [showDeclined, setShowDeclined] = useState(false);
+
   const fetchData = useCallback(async (accessToken) => {
     try {
       const { data } = await axios.get(
@@ -14,23 +18,38 @@ export default function App() {
           },
         }
       );
-      console.log(data);
-      setData(data);
+      setCalendarData({ accessToken: data });
     } catch (e) {}
   }, []);
+
   const fetchCalendarData = useCallback(async (accessToken) => {
     try {
       const { data } = await axios.get(
-        "https://www.googleapis.com/calendar/v3/calendars/primary/events",
+        `https://www.googleapis.com/calendar/v3/calendars/primary/events?orderBy=startTime&singleEvents=true&timeMin=${new Date(
+          new Date().setHours(0, 0, 0, 0)
+        ).toISOString()}&timeMax=${addDays(
+          new Date().setHours(0, 0, 0, 0),
+          4
+        ).toISOString()}`,
         {
           headers: {
             Authorization: "Bearer " + accessToken,
           },
         }
       );
-      console.log(data);
-      setData(data);
-    } catch (e) {}
+      setData(
+        data.items.reduce((acc, curr) => {
+          const date = format(new Date(curr.start.dateTime), "MM/dd/yy");
+          acc[date] = {
+            label: date,
+            items: [...(acc[date]?.items ?? []), curr],
+          };
+          return acc;
+        }, {})
+      );
+    } catch (e) {
+      console.log(e);
+    }
   }, []);
 
   // useEffect(() => {
@@ -38,7 +57,6 @@ export default function App() {
   // }, [fetchData]);
 
   const onSuccess = (param) => {
-    console.log("success", param);
     fetchData(param.accessToken);
     fetchCalendarData(param.accessToken);
   };
@@ -47,11 +65,19 @@ export default function App() {
     console.log("failure", param);
   };
 
+  const joinMeet = (meetingLink) => {
+    window.open(
+      meetingLink,
+      "MsgWindow",
+      "width=2000,height=750,top=100,status=0,menubar=0,location=0,titlebar=0,toolbar=0"
+    );
+  };
+
   const { signIn } = useGoogleLogin({
     onSuccess,
     onFailure,
     clientId: `452890721843-bvp31s2cq988jsiu9mlh83elp7cs8s1u.apps.googleusercontent.com`,
-    isSignedIn: false,
+    isSignedIn: true,
     accessType: "online",
     // discoveryDocs: [
     //   "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
@@ -65,41 +91,71 @@ export default function App() {
     // prompt: "consent",
   });
 
-  // useEffect(() => {
-  //   function start() {
-  //     // 2. Initialize the JavaScript client library.
-  //     gapi.client
-  //       .init({
-  //         apiKey: "YOUR_API_KEY",
-  //         // clientId and scope are optional if auth is not required.
-  //         clientId: "YOUR_WEB_CLIENT_ID.apps.googleusercontent.com",
-  //         scope: "profile"
-  //       })
-  //       .then(function () {
-  //         // 3. Initialize and make the API request.
-  //         return gapi.client.request({
-  //           path:
-  //             "https://people.googleapis.com/v1/people/me?requestMask.includeField=person.names"
-  //         });
-  //       })
-  //       .then(
-  //         function (response) {
-  //           console.log(response.result);
-  //         },
-  //         function (reason) {
-  //           console.log("Error: " + reason.result.error.message);
-  //         }
-  //       );
-  //   }
-  //   // 1. Load the JavaScript client library.
-  //   gapi.load("client", start);
-  // }, []);
-
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <button onClick={signIn}>Google</button>
-      <p>{JSON.stringify(data, null, 2)}</p>
+      <div style={{ display: "flex" }}>
+        <button onClick={signIn}>Login</button>
+        <label
+          style={{ display: "flex", marginLeft: "20px", cursor: "pointer" }}
+        >
+          <input
+            type="checkbox"
+            onChange={() => setShowDeclined((prev) => !prev)}
+            checked={showDeclined}
+          />
+          Show declined
+        </label>
+      </div>
+      {data &&
+        Object.values(data)?.map(({ label, items }) => (
+          <div key={label}>
+            {/* check if day doesn't only contain declined events */}
+            {(showDeclined ||
+              items.filter(
+                (item) =>
+                  !item.attendees.some(
+                    ({ self, responseStatus }) =>
+                      self && responseStatus === "declined"
+                  )
+              ).length > 0) && (
+              <h2>
+                {format(new Date(), "MM/dd/yy") === label
+                  ? "Today"
+                  : format(new Date(label), "iiii")}
+              </h2>
+            )}
+            <table>
+              <tbody>
+                {items?.map((item) =>
+                  // if showDeclined state is false then filter out declined events
+                  showDeclined ||
+                  !item.attendees.some(
+                    ({ self, responseStatus }) =>
+                      self && responseStatus === "declined"
+                  ) ? (
+                    <tr key={item.id}>
+                      <td>{item.summary} </td>
+                      <td>
+                        {isValid(new Date(item.start?.dateTime)) &&
+                          format(
+                            new Date(item.start?.dateTime),
+                            "dd/MM/yy hh:mm a"
+                          )}
+                      </td>
+                      <td>
+                        {item.hangoutLink && (
+                          <button onClick={() => joinMeet(item.hangoutLink)}>
+                            Join
+                          </button>
+                        )}
+                      </td>
+                    </tr>
+                  ) : null
+                )}
+              </tbody>
+            </table>
+          </div>
+        ))}
     </div>
   );
 }
